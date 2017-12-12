82688ac37a7f88a62046891cd693e98e3a328d9e
John J. Workman <john@helloeave.com>
2017-12-12 17:28:20 -0500

    Use pWorksheet.re to match

--- parser.go
+++ parser.go
@@ -113,7 +113,7 @@ var (
 func (p *parser) parseWorksheets() (map[string]*tWorksheet, error) {
 	wsDefs := make(map[string]*tWorksheet)
 
-	for p.peek() == pWorksheet.name {
+	for pWorksheet.re.MatchString(p.peek()) {
 		def, err := p.parseWorksheet()
 		if err != nil {
 			return nil, err
