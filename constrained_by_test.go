e850d4fdab5b9656843e21e2eb8dd3addb617d67
Alex Wilson <alex@helloeave.com>
2018-01-26 15:23:18 -0500

    one more test just for fun

--- constrained_by_test.go
+++ constrained_by_test.go
@@ -24,3 +24,17 @@ func (s *Zuite) TestWorksheet_constrainedBy() {
 	require.True(s.T(), ws.MustIsSet("name"))
 	require.Equal(s.T(), `"Alex"`, ws.MustGet("name").String())
 }
+
+func (s *Zuite) TestWorksheet_constrainedByNonsensicalExpression() {
+	defs, err := NewDefinitions(strings.NewReader(`worksheet constrained_nonsensical_constrained_expression {
+			69:some_field number[0] constrained_by { return some_field + 2 }
+	}`))
+	require.NoError(s.T(), err)
+
+	ws := defs.MustNewWorksheet("constrained_nonsensical_constrained_expression")
+
+	require.False(s.T(), ws.MustIsSet("some_field"))
+	err = ws.Set("some_field", MustNewValue("7"))
+	require.Equal(s.T(), "7 not a valid value for constrained field some_field", err.Error())
+	require.False(s.T(), ws.MustIsSet("some_field"))
+}
